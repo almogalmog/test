7f0eedb458cb97da763dfd9c315a4e99d11c6eae
malachico <malachicohen@gmail.com>
2015-04-23 10:28:00 +0300

    deleted interfaces

--- workshop/src/Domain/User_component/I_Guest.java
+++
@@ -1,9 +0,0 @@
-package Domain.User_component;
-
-import Domain.Forum_component.I_Forum;
-
-public interface I_Guest extends I_User{
-
-	public abstract I_Forum getForum();
-
-}
--- workshop/src/Domain/User_component/I_User.java
+++
@@ -1,9 +0,0 @@
-package Domain.User_component;
-
-public interface I_User {
-
-	public abstract String getName();
-
-	public abstract void setName(String name);
-
-}
